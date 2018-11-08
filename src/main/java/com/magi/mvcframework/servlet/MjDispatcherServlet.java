f20f9d3955cd56c802cce5a27cef685ca27d7a34
jingmei2 <daodaobendan@gmail.com>
2018-11-09 02:09:29 +0800

    add ioc DI maven Project

--- src/main/java/com/magi/mvcframework/servlet/MjDispatcherServlet.java
+++ src/main/java/com/magi/mvcframework/servlet/MjDispatcherServlet.java
@@ -1,5 +1,10 @@
 package com.magi.mvcframework.servlet;
 
+import com.magi.mvcframework.annotation.MjAutowired;
+import com.magi.mvcframework.annotation.MjController;
+import com.magi.mvcframework.annotation.MjRequestMapping;
+import com.magi.mvcframework.annotation.MjService;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,11 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * @author magi
@@ -21,7 +25,15 @@ public class MjDispatcherServlet extends HttpServlet{
 
     private Properties contextConfig = new Properties();
 
+    /**
+     * bean类的集合
+     */
     private List<String> classNames = new ArrayList<String>();
+
+    /**
+     * IOC 容器
+     */
+    private Map<String,Object> ioc = new HashMap<>();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -46,24 +58,134 @@ public class MjDispatcherServlet extends HttpServlet{
         doScanner(contextConfig.getProperty("scanPackage"));
 
         //3.初始化所有相关的类,并且保存到 IOC 容器之中
+        //通过反射机制,将实例化的对象放入 IOC 容器中
         doInstance();
 
         //4.完成自动化的依赖注入, DI
+        //在 IOC 容器中的实例,有很多属性没有复制,自动把需要的赋值的属性经行赋值
         doAutowired();
 
         //5.创建 HandlerMapping 将 URL和 method 建立对应关系
+        //能够将一个 URL 和一个 method 进行一个关联映射
         initHandlerMapping();
 
 
     }
 
     private void initHandlerMapping() {
+        if (ioc.isEmpty()){return;}
+        //遍历 IOC 容器中的实体
+        for (Map.Entry<String,Object> entry : ioc.entrySet()) {
+            Class<?> clazz = entry.getValue().getClass();
+
+            if (!clazz.isAnnotationPresent(MjController.class)){continue;}
+            String baseUrl ="";
+            //判断是否含有MjRequestMapping注解
+            if (clazz.isAnnotationPresent(MjRequestMapping.class)){
+                MjRequestMapping requestMapping = clazz.getAnnotation(MjRequestMapping.class);
+                baseUrl = requestMapping.value();
+
+            }
+
+            //开始扫描所有类 获取所有公共方法
+            Method [] methods = clazz.getMethods();
+
+
+
+
+        }
     }
 
     private void doAutowired() {
+        if (ioc.isEmpty()){return;}
+
+        for (Map.Entry<String,Object> entry : ioc.entrySet()){
+            //获取容器里 bean 类的字段数组
+            Field [] fields = entry.getValue().getClass().getDeclaredFields();
+            for (Field field : fields){
+                //判断字段上是否有注解(各种注解)
+                if (!field.isAnnotationPresent(MjAutowired.class)){
+                    continue;
+                }
+
+                MjAutowired autowired = field.getAnnotation(MjAutowired.class);
+                String beanName = autowired.value();
+                if ("".equals(beanName)){
+                    beanName = field.getType().getName();
+                }
+                //如果是 private 的话 是不可见,需要强制可见
+                field.setAccessible(true);
+                try {
+                    //给字段设置, key 和注入的对象
+                    field.set(entry.getValue(),ioc.get(beanName));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+
+
     }
 
     private void doInstance() {
+        if (classNames.isEmpty()){return;}
+
+        //开始反射
+        try {
+            for (String className : classNames){
+                Class<?> clazz = Class.forName(className);
+
+                //判断需要实例化的class
+                if (clazz.isAnnotationPresent(MjController.class)){
+                    //取单纯的类名 不含有包名
+                    String beanName = lowerFristCase(clazz.getSimpleName());
+                    ioc.put(beanName,clazz.newInstance());
+
+
+
+                } else if (clazz.isAnnotationPresent(MjService.class)){
+
+                    //1. 默认类名首字母小写
+                    //2. 自定义的命名
+                    MjService service = clazz.getAnnotation(MjService.class);
+                    String beanName = service.value();
+                    if ("".equals(beanName)){
+                        beanName = lowerFristCase(clazz.getSimpleName());
+                    }
+
+                    Object instance = clazz.newInstance();
+                    ioc.put(beanName,instance);
+
+                    //3. 注入接口的实现类,用接口的全称作为 key, 用接口的实现类实例作为值
+                    //实现接口可能有多个,是为数组
+                    Class<?> [] interfaces = clazz.getInterfaces();
+                    for (Class ifs : interfaces){
+                        if (ioc.containsKey(ifs.getName())){
+                            throw new Exception("The beanName is exists:"+ifs.getName());
+                        }
+//                        String interFaceName = interFaces.getSimpleName();
+//
+//                        Object obj = interFaces.getSuperclass().newInstance();
+                        ioc.put(ifs.getName(),instance);
+
+                    }
+
+                } else {
+                    continue;
+                }
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private String lowerFristCase(String simpleName) {
+        char [] chars = simpleName.toCharArray();
+        chars[0] += 32;
+        return chars.toString();
     }
 
     private void doScanner(String scanPackage) {
